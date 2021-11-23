280dbf8f62dd4fc23e6e4133dce577b2834bc449
Alexis Massa <alexis.massa351@gmail.com>
2021-11-23 11:43:03 +0100

    partie 1 : message de bienvenue

--- sujet/TP3/partie1/guess.js
+++ sujet/TP3/partie1/guess.js
@@ -1,27 +1,28 @@
 window.onload = () => {
 
-    var toGuess = Math.floor(Math.random() * 10) + 1;
+    var pays = {
+        "France": ["Lille", "Brest", "Nice", "Pau"],
+        "Italie": ["Rome", "Turin", "Milan", "Venise"],
+        "Allemagne": ["Munich", "Berlin", "Nuremberg", "Stuttgart"],
+        "Espagne": ["Barcelone", "Valence", "Seville", "Malaga"]
+    };
 
-    var guess = -1;
-    var tries = 3;
-    while (guess !== toGuess && tries > 0 || guess == -1) {
-        if (isNaN(guess) || guess === null || guess == -1) {
-            guess = Number(window.prompt("Devinez le nombre. (" + tries + " éssais restants)", ""));
-        } else {
-            tries--;
-            if (guess < toGuess) {
-                guess = Number(window.prompt("Plus grand. (" + tries + " éssais restants)", ""));
-            } else if (guess > toGuess) {
-                guess = Number(window.prompt("Plus petit. (" + tries + " éssais restants)", ""));
+    document.getElementById('form').addEventListener("submit", () => {
+        event.preventDefault();
+        var submitted = document.getElementById('city').value;
+        var country = trouverPays(submitted)
+        if (country)
+            alert("Bienvenue à " + submitted + " (" + country + ").");
+        else
+            alert("Non trouvée");
+    });
+
+    function trouverPays(submitted) {
+        for (key in pays) {
+            if (pays[key].includes(submitted)) {
+                return key
             }
         }
+        return false;
     }
-    if (tries == 0)
-        alert("Vous n'avez plus d'éssais.. La bonne réponse était : " + toGuess);
-    else {
-        var score = 4 - tries;
-        alert("Vous avez deviné en " + score + " éssais! La réponse était : " + toGuess);
-    }
-
-    // location.reload();
 }
